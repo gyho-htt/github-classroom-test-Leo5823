b030a3f2e1911f0d9a18aeb70931af9002727e5e
Leonard Glamsch <leonard.glamsch@gyho-hattingen.de>
2025-11-04 08:59:46 +0100

    Version2

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
   public static void main(String[] args){
     System.out.println("Hello World");
     System.out.println("Hier ist Leonard");
+    System.out.println("Ein weiterer kleiner test");
 }
 }
